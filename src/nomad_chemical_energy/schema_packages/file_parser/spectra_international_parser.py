0cdd85402283a86ff716e4103591626a315b5a75
Micha <michael.goette@posteo.de>
2024-08-19 14:14:14 +0200

    error handling for skipped value

--- src/nomad_chemical_energy/schema_packages/file_parser/spectra_international_parser.py
+++ src/nomad_chemical_energy/schema_packages/file_parser/spectra_international_parser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def to_float(value):
@@ -25,8 +26,10 @@ def parse_spectrum(file):
 
         data = pd.read_csv(f, sep="\t", header=0, on_bad_lines="skip")
         data.Time = pd.to_datetime(data.Time, errors="coerce")
-    return metadata, data[pd.notnull(data.Time)]
+        data = data[pd.notnull(data.Time)]
+        data[data == "Skipped"] = np.nan
+    return metadata, data
 
 
-# file = "ce-nome/frederik/test double PTFETape 3 100724000001.txt"
+# file = "ce-nome/frederik/CE-NOME_FrJo_240815_0001.0 H2O Au000001.txt"
 # m, d = parse_spectrum(file)
