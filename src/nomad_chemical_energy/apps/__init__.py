052cea8bf2c73772b893fd04d362e9282a7e784b
Carla Terboven <158454895+carla-terboven@users.noreply.github.com>
2025-02-28 12:35:31 +0100

    Add button in search app to jump to file browser for easy launch (#49)
    * Add button to file browser for easy launch
    
    * Add parameters to fix pytest
    
    * ruff
    
    * linting
    
    ---------
    
    Co-authored-by: Michael Götte <36420750+RoteKekse@users.noreply.github.com>
    Co-authored-by: Micha <michael.goette@posteo.de>

--- src/nomad_chemical_energy/apps/__init__.py
+++ src/nomad_chemical_energy/apps/__init__.py
@@ -5,11 +5,107 @@ from nomad.config.models.ui import (
     Columns,
     FilterMenu,
     FilterMenus,
+    FilterMenuSizeEnum,
     Filters,
+    Format,
+    ModeEnum,
+    RowActions,
+    RowActionURL,
+    RowDetails,
+    Rows,
+    RowSelection,
 )
 
 from nomad_chemical_energy.apps.catlab_combinatorial_app import catlab_combinatorial_app
 from nomad_chemical_energy.apps.necc_find_app import necc_find_experiments_app
+
+schema_name = (
+    'nomad_chemical_energy.schema_packages.ce_nome_package.CE_NOME_VoilaNotebook'
+)
+nome_documentation_app = AppEntryPoint(
+    name='voila',
+    description='Find and launch your Voila Tools.',
+    app=App(
+        # Label of the App
+        label='Voila',
+        # Path used in the URL, must be unique
+        path='voila',
+        # Used to categorize apps in the explore menu
+        category='use cases',
+        # Brief description used in the app menu
+        description='Find and launch your Voila Tools.',
+        # Longer description that can also use markdown
+        readme='Find and launch your Voila Tools.',
+        # Controls the available search filters. If you want to filter by
+        # quantities in a schema package, you need to load the schema package
+        # explicitly here. Note that you can use a glob syntax to load the
+        # entire package, or just a single schema from a package.
+        filters=Filters(
+            include=[
+                '*#nomad_chemical_energy.schema_packages.ce_nome_package.CE_NOME_VoilaNotebook',
+            ]
+        ),
+        # Dictionary of search filters that are always enabled for queries made
+        # within this app. This is especially important to narrow down the
+        # results to the wanted subset. Any available search filter can be
+        # targeted here. This example makes sure that only entries that use
+        # MySchema are included.
+        filters_locked={
+            'section_defs.definition_qualified_name': 'nomad_chemical_energy.schema_packages.ce_nome_package.CE_NOME_VoilaNotebook'
+        },
+        filter_menus=FilterMenus(
+            options={
+                'custom_quantities': FilterMenu(
+                    label='Notebooks', size=FilterMenuSizeEnum.L
+                ),
+                'author': FilterMenu(label='Author', size=FilterMenuSizeEnum.M),
+                'metadata': FilterMenu(label='Visibility / IDs'),
+            }
+        ),
+        columns=[
+            Column(quantity=f'data.name#{schema_name}', selected=True),
+            Column(
+                quantity='entry_type', label='Entry type', align='left', selected=True
+            ),
+            Column(
+                quantity='entry_create_time',
+                label='Entry time',
+                align='left',
+                selected=True,
+                format=Format(mode=ModeEnum.DATE),
+            ),
+            Column(
+                quantity='upload_name',
+                label='Upload name',
+                align='left',
+                selected=True,
+            ),
+            Column(
+                quantity='authors',
+                label='Authors',
+                align='left',
+                selected=True,
+            ),
+            Column(quantity='entry_id'),
+            Column(quantity='upload_id'),
+            Column(quantity=f'data.notebook_file#{schema_name}'),
+        ],
+        rows=Rows(
+            actions=RowActions(
+                items=[
+                    RowActionURL(
+                        path=f'data.file_uri#{schema_name}',
+                        description='View in file browser',
+                        icon='search',
+                    ),
+                ]
+            ),
+            details=RowDetails(),
+            selection=RowSelection(),
+        ),
+    ),
+)
+
 
 catlab_combinatorial_library_app = AppEntryPoint(
     name='Compinatorial Samples',
