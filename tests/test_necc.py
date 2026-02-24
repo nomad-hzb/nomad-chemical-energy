4308668d53238b25d03ec4fe458c43b76cd7648a
carla-terboven <carla.terboven@helmholtz-berlin.de>
2026-02-24 14:35:00 +0100

    Restructure monkeypatch

--- tests/test_necc.py
+++ tests/test_necc.py
@@ -1,48 +1,10 @@
 from nomad.client import normalize_all, parse
 
 
-def set_monkey_patch(monkeypatch):
-    def mockreturn_search(*args):
-        return None
-
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.search_class', mockreturn_search
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.find_sample_by_id',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_necc_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.hzb_general_parser.update_general_process_entries',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.schema_packages.tfc_package.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nesd_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.schema_packages.ce_nesd_package.create_archive',
-        mockreturn_search,
-    )
-
-
-def test_recipe_entry_hash(monkeypatch):
+def test_recipe_entry_hash():
     """
     Test for entry_dict_hash to be only equal for recipes with identical content data.
     """
-    set_monkey_patch(monkeypatch)
     file = 'tests/data/archives/necc_test_recipe.archive.json'
     recipe1_archive = parse(file)[0]
     recipe2_archive = parse(file)[0]
