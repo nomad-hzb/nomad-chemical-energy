ee8e7f7522e5ab60db540bdb7fdb00efe604cd74
carla-terboven <carla.terboven@helmholtz-berlin.de>
2026-02-24 14:34:04 +0100

    rebase for add test and newest ec-gc-excel-template as testfile

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,44 +1,8 @@
 import os
 
+import pandas as pd
 import pytest
 from nomad.client import normalize_all, parse
-
-
-def set_monkey_patch(monkeypatch):
-    def mockreturn_search(*args):
-        return None
-
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.search_class', mockreturn_search
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nome_parser.find_sample_by_id',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_necc_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.hzb_general_parser.update_general_process_entries',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.schema_packages.tfc_package.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.parsers.ce_nesd_parser.set_sample_reference',
-        mockreturn_search,
-    )
-    monkeypatch.setattr(
-        'nomad_chemical_energy.schema_packages.ce_nesd_package.create_archive',
-        mockreturn_search,
-    )
 
 
 @pytest.fixture(
@@ -63,6 +27,7 @@ def set_monkey_patch(monkeypatch):
         'LSVCA.DTA',
         'LSV.DTA',
         'necc_test_no_properties.xlsx',
+        '20260206_necc_ec_gc_template.xlsx',
         'NOME_RRDE_CVCA.DTA',
         'OCP.DTA',
         'P-O2D109.pump.csv',
@@ -87,13 +52,10 @@ def set_monkey_patch(monkeypatch):
         '11_PEIS_700mV.pssession',
     ]
 )
-def parsed_archive(request, monkeypatch):
+def parsed_archive(request):
     """
     Sets up data for testing and cleans up after the test.
     """
-
-    set_monkey_patch(monkeypatch)
-
     rel_file = os.path.join('tests', 'data', request.param)
     file_archive = parse(rel_file)[0]
     measurement = os.path.join('tests', 'data', request.param + '.archive.json')
@@ -114,19 +76,18 @@ def parsed_archive(request, monkeypatch):
     assert archive_json
 
 
-def test_nome_ocp_parser(monkeypatch):
+def test_nome_ocp_parser():
     file = 'ref_calibration.DTA'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'Open' in str(archive.data.m_def)
 
 
-def test_normalize_all(parsed_archive, monkeypatch):
+def test_normalize_all(parsed_archive):
     normalize_all(parsed_archive)
 
 
-def get_archive(file_base, monkeypatch):
-    set_monkey_patch(monkeypatch)
+def get_archive(file_base):
     file_name = os.path.join('tests', 'data', file_base)
     parse(file_name)[0]
     for file in os.listdir(os.path.join('tests/data')):
@@ -142,8 +103,7 @@ def get_archive(file_base, monkeypatch):
     return measurement_archive
 
 
-def get_multiple_archives(file_base, monkeypatch):
-    set_monkey_patch(monkeypatch)
+def get_multiple_archives(file_base):
     file_name = os.path.join('tests', 'data', file_base)
     parse(file_name)[0]
     measurement_archive_list = []
@@ -159,9 +119,9 @@ def get_multiple_archives(file_base, monkeypatch):
     return measurement_archive_list
 
 
-def test_biologic_eclab_constC_parser(monkeypatch):
+def test_biologic_eclab_constC_parser():
     file = 'CstC_nesd.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert archive.data.properties.lower_limit_potential.magnitude == -2.5
@@ -171,9 +131,9 @@ def test_biologic_eclab_constC_parser(monkeypatch):
     )
 
 
-def test_biologic_eclab_constV_parser(monkeypatch):
+def test_biologic_eclab_constV_parser():
     file = 'CstV_nesd.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert archive.data.properties.sample_period.magnitude == 5
@@ -183,9 +143,9 @@ def test_biologic_eclab_constV_parser(monkeypatch):
     )
 
 
-def test_biologic_eclab_GEIS_parser(monkeypatch):
+def test_biologic_eclab_GEIS_parser():
     file = 'GEIS_nesd.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.description == 'abc'
     assert archive.data.measurements
@@ -197,9 +157,9 @@ def test_biologic_eclab_GEIS_parser(monkeypatch):
     )
 
 
-def test_biologic_eclab_LSV_parser(monkeypatch):
+def test_biologic_eclab_LSV_parser():
     file = 'LSV_nesd.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert archive.data.properties.final_potential.magnitude == 1
@@ -209,9 +169,9 @@ def test_biologic_eclab_LSV_parser(monkeypatch):
     )
 
 
-def test_biologic_eclab_PEIS_parser(monkeypatch):
+def test_biologic_eclab_PEIS_parser():
     file = 'PEIS_nesd.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.description == 'abc'
     assert archive.data.measurements
@@ -223,9 +183,9 @@ def test_biologic_eclab_PEIS_parser(monkeypatch):
     )
 
 
-def test_gamry_EIS_parser(monkeypatch):
+def test_gamry_EIS_parser():
     file = 'EISPOT.DTA'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert archive.data.frequency[0]
@@ -234,9 +194,9 @@ def test_gamry_EIS_parser(monkeypatch):
     assert archive.data.properties.initial_frequency.magnitude == 2 * 1e6
 
 
-def test_gamry_CV_parser(monkeypatch):
+def test_gamry_CV_parser():
     file = 'CV.DTA'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert archive.data.cycles[0].voltage[0]
@@ -244,9 +204,9 @@ def test_gamry_CV_parser(monkeypatch):
     assert archive.data.properties.limit_potential_1.magnitude == 0.5
 
 
-def test_gamry_RRDE_parser(monkeypatch):
+def test_gamry_RRDE_parser():
     file = 'NOME_RRDE_CVCA.DTA'
-    rrde_archives = get_multiple_archives(file, monkeypatch)
+    rrde_archives = get_multiple_archives(file)
     assert len(rrde_archives) == 2
     if rrde_archives[0].data.method == 'Chronoamperometry':
         ca_archive, cv_archive = rrde_archives
@@ -266,9 +226,9 @@ def test_gamry_RRDE_parser(monkeypatch):
     assert cv_archive.data.properties.limit_potential_1.magnitude == -1.0402
 
 
-def test_kmc3_parser(monkeypatch):
+def test_kmc3_parser():
     file = 'xas_kmc3_example.001'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.sdd_parameters
     assert round(archive.data.energy[0].magnitude, 4) == 6.4140
@@ -276,9 +236,9 @@ def test_kmc3_parser(monkeypatch):
     assert round(archive.data.sdd_parameters[1].slope, 4) == 0.9958
 
 
-def test_kmc3_parser_new_header(monkeypatch):
+def test_kmc3_parser_new_header():
     file = 'xas_kmc3_new_header.0003'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.sdd_parameters
     assert archive.data.energy[0].magnitude == 8.18551
@@ -287,9 +247,9 @@ def test_kmc3_parser_new_header(monkeypatch):
     assert archive.data.quality_annotation == 'ICR within specified bounds'
 
 
-def test_kmc3_insitu_biologic_parser(monkeypatch):
+def test_kmc3_insitu_biologic_parser():
     file = 'kmc3_biologic_CA_example.mpr'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties
     assert len(archive.data.time) == 600
@@ -297,98 +257,98 @@ def test_kmc3_insitu_biologic_parser(monkeypatch):
     assert round(archive.data.voltage[2].magnitude, 5) == -0.44958
 
 
-def test_nesd_metadata_excel_parser(monkeypatch):
+def test_nesd_metadata_excel_parser():
     file = 'nesd_metadata_example.xlsx'
-    archive_list = get_multiple_archives(file, monkeypatch)
+    archive_list = get_multiple_archives(file)
     assert len(archive_list) == 3
 
 
-def test_labview_nesd_parser(monkeypatch):
+def test_labview_nesd_parser():
     file = 'labview_metadata_nesd.tdms'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert len(archive.data.time) == 344
     assert archive.data.name == '20241202_091736_Softwaretest001_001'
 
 
-def test_zahner_isw_nesd_parser(monkeypatch):
+def test_zahner_isw_nesd_parser():
     file = '22-cp-1700mv-10min.isw'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'chronoamperometry' in str(archive.data.m_def).lower()
     assert len(archive.data.time) == 601
 
 
-def test_zahner_isw_2_nesd_parser(monkeypatch):
+def test_zahner_isw_2_nesd_parser():
     file = '21-cp-625ma-5min.isw'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'chronopotentiometry' in str(archive.data.m_def).lower()
     assert len(archive.data.time) == 301
 
 
-def test_zahner_isw_3_nesd_parser(monkeypatch):
+def test_zahner_isw_3_nesd_parser():
     file = '25-currentscan3.isw'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'galvanodynamicsweep' in str(archive.data.m_def).lower()
     assert len(archive.data.time) == 282
 
 
-def test_zahner_ism_nesd_parser(monkeypatch):
+def test_zahner_ism_nesd_parser():
     file = 'geis-100ma.ism'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'geis' in str(archive.data.m_def).lower()
     assert len(archive.data.measurements[0].data.frequency) == 33
 
 
-def test_zahner_ism_2_nesd_parser(monkeypatch):
+def test_zahner_ism_2_nesd_parser():
     file = '02_peis_ocv.ism'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'peis' in str(archive.data.m_def).lower()
     assert len(archive.data.measurements[0].data.frequency) == 66
 
 
-def test_zahner_isc_nesd_parser(monkeypatch):
+def test_zahner_isc_nesd_parser():
     file = 'pt-wire-cv-.isc'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'cyclicvolt' in str(archive.data.m_def).lower()
     assert len(archive.data.cycles[0].current) == 446
     assert round(archive.data.properties.limit_potential_1.magnitude, 5) == 0.6
 
 
-def test_chi_txt_lsv_nesd_parser(monkeypatch):
+def test_chi_txt_lsv_nesd_parser():
     file = 'LSV.txt'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.properties.scan_rate.magnitude == 5
     assert 'linear' in str(archive.data.m_def).lower()
     assert round(archive.data.voltage[0].magnitude, 5) == -0.2
 
 
-def test_chi_txt_eis_nesd_parser(monkeypatch):
+def test_chi_txt_eis_nesd_parser():
     file = 'EIS -0,48 V.txt'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'peis' in str(archive.data.m_def).lower()
     assert len(archive.data.measurements[0].data.frequency) == 73
 
 
-def test_chi_txt_cv_nesd_parser(monkeypatch):
+def test_chi_txt_cv_nesd_parser():
     file = 'CV-1T.txt'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'cyclicvolt' in str(archive.data.m_def).lower()
     assert len(archive.data.cycles[0].current) == 1400
     assert round(archive.data.properties.limit_potential_1.magnitude, 5) == 0.2
 
 
-def test_palmense_lsv_nesd_parser(monkeypatch):
+def test_palmense_lsv_nesd_parser():
     file = '05_N2_LSV_10mV_1600rpm.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'linear' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1752740575
@@ -396,9 +356,9 @@ def test_palmense_lsv_nesd_parser(monkeypatch):
     assert round(archive.data.voltage[0].magnitude, 5) == -0.09996
 
 
-def test_palmense_ocp_nesd_parser(monkeypatch):
+def test_palmense_ocp_nesd_parser():
     file = '01_N2_OCP.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'open' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1752739293
@@ -406,9 +366,9 @@ def test_palmense_ocp_nesd_parser(monkeypatch):
     assert round(archive.data.voltage[0].magnitude, 5) == 0.96293
 
 
-def test_palmense_cv_nesd_parser(monkeypatch):
+def test_palmense_cv_nesd_parser():
     file = '04_N2_CV_50mV.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'cyclic' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1752739815
@@ -416,9 +376,9 @@ def test_palmense_cv_nesd_parser(monkeypatch):
     assert round(archive.data.cycles[0].voltage[0].magnitude, 5) == 0.02496
 
 
-def test_palmense_peis_nesd_parser(monkeypatch):
+def test_palmense_peis_nesd_parser():
     file = '11_PEIS_700mV.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'peis' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1752741445
@@ -426,9 +386,9 @@ def test_palmense_peis_nesd_parser(monkeypatch):
     assert round(archive.data.measurements[0].data.frequency[0].magnitude, 5) == 200000
 
 
-def test_palmense_cp_nesd_parser(monkeypatch):
+def test_palmense_cp_nesd_parser():
     file = '05_N2_CP_10mAcm2_24h.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'chronopotentiometry' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1752495542
@@ -436,9 +396,9 @@ def test_palmense_cp_nesd_parser(monkeypatch):
     assert round(archive.data.voltage[0].magnitude, 5) == 0.65457
 
 
-def test_palmense_ca_nesd_parser(monkeypatch):
+def test_palmense_ca_nesd_parser():
     file = 'palmsens_ca.pssession'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert 'chronoamperometry' in str(archive.data.m_def).lower()
     assert round(archive.data.datetime.timestamp(), 0) == 1738771707
@@ -446,9 +406,9 @@ def test_palmense_ca_nesd_parser(monkeypatch):
     assert round(archive.data.current[0].magnitude, 5) == 0.00179
 
 
-def test_tfc_sputtering_parser(monkeypatch):
+def test_tfc_sputtering_parser():
     file = 'tfc_sputtering.xlsx'
-    archive = get_archive(file, monkeypatch)
+    archive = get_archive(file)
     assert archive.data
     assert archive.data.targets
     assert archive.data.process_properties
@@ -457,3 +417,31 @@ def test_tfc_sputtering_parser(monkeypatch):
     assert archive.data.process_properties[0].sputter_pressure.magnitude == 0.0167
     assert archive.data.targets[0].name == '1) Al'
     assert archive.data.observables[0].temperature.magnitude == 25
+
+
+def test_necc_ecgc_excel():
+    file = '20260206_necc_ec_gc_template.xlsx'
+    archive = get_archive(file)
+    normalize_all(archive)
+    assert archive.data
+    assert archive.data.properties
+    assert archive.data.properties.chronoanalysis_method == 'CP'
+    assert archive.data.properties.bleedline_flow_rate.magnitude == 10
+    assert archive.data.properties.feed_gases[0].name == 'CO'
+    assert len(archive.data.gaschromatographies) == 6
+    h2 = archive.data.gaschromatographies[4]
+    assert h2.gas_type == 'H2'
+    assert round(h2.ppm[2].magnitude, 4) == 31674.4523
+    assert archive.data.potentiometry
+    assert len(archive.data.potentiometry.current) == 10145
+    assert archive.data.potentiometry.maximum_current.magnitude == 800.416
+    assert archive.data.potentiometry.datetime[0] == pd.Timestamp(
+        '2025-12-17 13:05:28.433000+0000', tz='UTC'
+    )
+    assert archive.data.fe_results
+    assert archive.data.fe_results
+    assert archive.data.ph
+    assert archive.data.ph.datetime[0] == pd.Timestamp(
+        '2025-12-17 12:59:10+0000', tz='UTC'
+    )
+    assert archive.data.ph.ph_value[0] == 11.514
