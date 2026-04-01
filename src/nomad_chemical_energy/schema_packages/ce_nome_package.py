cd71b8284ddc58aa92a9f53bed270f697344431f
Asaph Hall <aimlikefridge@gmail.com>
2026-04-01 18:16:41 +0200

    removed link from CE_NOME_Chronocoulometry

--- src/nomad_chemical_energy/schema_packages/ce_nome_package.py
+++ src/nomad_chemical_energy/schema_packages/ce_nome_package.py
@@ -1273,7 +1273,6 @@ class CE_NOME_Chronopotentiometry(Chronopotentiometry, EntryData):
 
 class CE_NOME_Chronocoulometry(Chronocoulometry, EntryData):
     m_def = Section(
-        links=['https://w3id.org/nfdi4cat/voc4cat_0007212'],
         a_eln=dict(
             hide=[
                 'lab_id',
